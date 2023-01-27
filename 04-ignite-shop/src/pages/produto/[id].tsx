757ef294de50beefe4aeabc69d1b8bd8e96c68f1
Thales Sousa <thalestjsb@gmail.com>
2023-01-27 15:55:43 -0300

    Carregando dados do produto

--- 04-ignite-shop/src/pages/produto/[id].tsx
+++ 04-ignite-shop/src/pages/produto/[id].tsx
@@ -1,22 +1,63 @@
+import { GetStaticProps } from "next"
+import Image from "next/image"
 import { useRouter } from "next/router"
+
+import Stripe from "stripe"
+import { stripe } from "../../lib/stripe"
+
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
-export default function Product() {
-  const {query} = useRouter()
+interface IProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  }
+}
+
+export default function Product({ product } :IProductProps) {
   return (
     <ProductContainer>
       <ImageContainer>
-
+        <Image src={product.imageUrl} width={520} height={400} alt="" />
       </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta 1</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam aliquam repellat earum magnam voluptates temporibus officiis enim quod rem voluptate.</p>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default.price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+      },
+      description: product.description
+    },
+    revalidate: 60 * 60 * 1,
+  }
 }
