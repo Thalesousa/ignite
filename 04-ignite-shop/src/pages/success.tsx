9572f8383ebb99532792459e1e6ee57488ca2143
Thales Sousa <thalestjsb@gmail.com>
2023-02-06 16:55:25 -0300

    Estrutura de sucesso

---
+++ 04-ignite-shop/src/pages/success.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import { SuccessContainer, ImageContainer } from "../styles/pages/success";
+
+export default function Success() {
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>John Doe</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
+  )
+}
