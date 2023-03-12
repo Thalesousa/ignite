65e4f68b7885df9ff5d2f548aa3d2208c0439ad8
Thalesousa <thalestjsb@gmail.com>
2023-03-12 08:07:39 -0300

    Projeto Storybook

--- 05-design-system/packages/docs/src/stories/Box.stories.tsx
+++ 05-design-system/packages/docs/src/stories/Box.stories.tsx
@@ -1,12 +1,19 @@
-import type { Meta, StoryObj } from "@storybook/react";
-import { Box, BoxProps, Text } from "@ignite-ui/react";
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, BoxProps, Text } from '@ignite-ui/react'
 
 export default {
-  title: "Surfaces/Box",
+  title: 'Surfaces/Box',
   component: Box,
   args: {
     children: <Text>Testando o elemento Box</Text>,
   },
-} as Meta<BoxProps>;
+  argTypes: {
+    children: {
+      control: {
+        type: null,
+      },
+    },
+  },
+} as Meta<BoxProps>
 
-export const Primary: StoryObj<BoxProps> = {};
+export const Primary: StoryObj<BoxProps> = {}
